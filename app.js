ebdd61a81c925f6d2bc8e673a1f6629f6a021a45
ErdalDomi <erdal.domi2@gmail.com>
2017-04-30 15:43:45 +0200

    first server structure

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+var bodyParser = require('body-parser')
+var Client = require('pg').Client;
+var connection = require('pg').Connection;
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended:true}));  // to support URL-encoded bodies
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Start the app
+var port = 8000;
+
+var onServerStart = function() {
+  console.log("Listening on port " + port);
+}
+
+app.listen(port,onServerStart);
+
+//-----------------db connection stub ------------------//
+console.log("Connecting to the db...");
+var username = 'postgres'; //request.body.username;
+var password = 'password'; //request.body.password;
+var dbname = 'infsec'; //request.body.dbname;
+client = new Client({
+    user: username,
+    password: password,
+    database: dbname,
+    host: '127.0.0.1',
+    port: 5432
+  });
+connection = client.connect(function(err){
+  if(err){
+    console.log("error connecting ", err);
+  }
+});
+console.log('Connected to '+dbname + ' as user ' +username);
