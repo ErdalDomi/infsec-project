0e5ce340fd7898540331b38f9ddf8d8f40875151
ErdalDomi <erdal.domi2@gmail.com>
2017-06-04 02:20:28 +0200

    ui almost finished + user auth

--- app.js
+++ app.js
@@ -1,26 +1,103 @@
 var express = require('express')
 var app = express()
+
 var path = require('path')
 var bodyParser = require('body-parser')
-
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var session      = require('express-session');
-var mongoose = require('mongoose');
-var User = require('./user.js');
 var bcrypt = require('bcrypt');
+
+//tls
+// var fs = require('fs');
+// var https = require('https');
+// var options = {
+//    key  : fs.readFileSync('server.key'),
+//    cert : fs.readFileSync('server.crt')
+// };
+
+var psql = require('pg');
+var Client = require('pg').Client;
+// var psqlConnection = require('pg').Connection;
 
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended:true}));  // to support URL-encoded bodies
 app.use(express.static(path.join(__dirname, 'public')));
+//////////////////////////////////////////////////////////////////////////////
 
-mongoose.connect('mongodb://localhost/jewellry');
+var client; //we use this to query the database
 
-var chris = new User({username: 'chris', password: 'password'});
+client = new Client({
+  user: 'postgres',
+  password: 'password',
+  database: 'infsec',
+  host: '127.0.0.1',
+  port: 5432
+});
+client.connect();
+client.on('error', function(error){
+  console.log('error connecting to the db: ' + error);
+});
 
-console.log("generating password " + chris.generateHash(this.password));
+console.log('Database connection established');
+
+/////-----------------------
+
+app.post('/signup', function(req,res){
+  //stub here securely get user credentials
+  var email = req.body.email;
+  var password = req.body.password;
+
+  var query = client.query("select * from users where email = '"+email+"';"); //do prepared statements here
+  query.on('error', function(error){
+    console.log("there was an error cheking existing users. " + error);
+  });
+  query.on('end', function(result){
+    if(result.rows[0]){
+      res.send('no');
+    }else{
+      var hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+      var query = client.query("insert into users(email, password) values ('"+email+"' , '"+hashedPassword+"');"); //do prepared statements here
+      query.on('error', function(error){
+        console.log("there was an error creating user. " + error);
+      });
+      res.send('ok');
+    }
+  });
+
+});
+
+app.post('/login', function(req,res){
+  //stub here securely get user credentials
+  var email = req.body.email;
+  var password = req.body.password;
+
+  console.log("the server got these credentials: " + email + " and " + password);
+
+  var query = client.query("select * from users where email = '"+email+"';"); //do prepared statements here
+  query.on('error', function(error){
+    console.log("there was an error retreiving user. " + error);
+  });
+  query.on('end', function(result){
+    if(result.rows[0]){
+      var check = bcrypt.compareSync(password, result.rows[0].password);
+      if(check){
+        res.send('ok');
+      }else{
+        res.send('no');
+      }
+    }
+    else{
+      res.send('no');
+    }
+  });
+
+
+});
+
+
 
 //Start the app
 var port = 3000;
--- public/javascript/layout.js
+++ public/javascript/layout.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
 
+
   $('.special.cards .image').dimmer({
     on: 'hover'
   });
 
-  $('.ui.sidebar').sidebar();
+
   $('.ui.accordion').accordion();
   $('.ui.accordion').accordion({exclusive:false})
   $('.ui.dropdown').dropdown();
@@ -17,14 +18,14 @@ $( document ).ready(function() {
     }
   });
 
-  $('.ui.sidebar').sidebar({
-    onShow : function(){
-      $('body').css('overflow', 'hidden');
-    },
-    onHidden : function(){
-      $('body').css('overflow', 'initial');
-    }
-  });
+  // $('.ui.sidebar').sidebar({
+  //   onShow : function(){
+  //     $('body').css('overflow', 'hidden');
+  //   },
+  //   onHidden : function(){
+  //     $('body').css('overflow', 'initial');
+  //   }
+  // });
 
   $('#miniLogo').hide();
 
@@ -81,39 +82,59 @@ $( document ).ready(function() {
   $('#photoSlider').css('visibility','visible');
   $('.bxslider').bxSlider();
 
+  // $('.ui.sidebar').module({selector: {ommited: '.dimmer'}}).sidebar();
+  $.fn.modal.settings.context = '#container';
+  //$.fn.sidebar.settings.context ='#shoppingCart';
 });
 
+function showSidebar(){
+  $('.ui.sidebar').sidebar({
+    onShow : function(){
+      $('body').css('overflow', 'hidden');
+    },
+    onHidden : function(){
+      $('body').css('overflow', 'initial');
+    }
+  });
+  $('#shoppingCart').sidebar('toggle');
+}
+
+
 function showSignInModal(){
-  $('#loginModal').modal({
-      blurring: true
-    }).modal('show');
+  $('#loginModal').modal('show');
 }
 
 function showRegisterModal(){
   $('#registerModal')
-    .modal({
-      blurring: true
-    })
+    // .modal({
+    //   blurring: true
+    // })
     .modal('show')
   ;
 }
 
 function itemModal(itemName){
-  $('#itemModal').modal({blurring: true}).modal('show');
+  $('#itemModal').modal('show');
   $('#testItem').text(itemName);
 }
 
 
 
-function signIn(){
-  console.log("in the sign in function");
+function logIn(){
+
+  console.log("in the log in function");
   var email = document.getElementsByName('email')[0].value;
   var password = document.getElementsByName('password')[0].value;
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-
+      if(this.responseText === 'ok'){
+        document.getElementsByName('email')[0].value='';
+        document.getElementsByName('password')[0].value='';
+        $('#loginModal').modal('hide');
+      }
+      console.log("server said: " + this.responseText);
     }
   }
 
@@ -122,14 +143,49 @@ function signIn(){
   xhttp.send('email='+email+'&password='+password);
 }
 
+function signUp(){
+
+  var xhttp = new XMLHttpRequest();
+  var regEmail = document.getElementsByName('regEmail')[0].value;
+  var regPassword = document.getElementsByName('regPassword')[0].value;
+  var regPasswordConfirm = document.getElementsByName('regPasswordConfirm')[0].value;
+
+  if(regPassword == regPasswordConfirm){
+    xhttp.open("post", "/signup", true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send('email='+regEmail+'&password='+regPassword);
+  }else{
+    console.log("passwords don't match");
+  }
+
+
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      console.log("server said: " + this.responseText);
+      if(this.responseText === 'ok'){
+        document.getElementsByName('regEmail')[0].value = '';
+        document.getElementsByName('regPassword')[0].value = '';
+        //... and the other input field (confirm password)
+        $('#registerModal').modal('hide');
+        //give user a thhumbs
+      }
+
+    }
+  }
+
+}
+
 function addToCart(){
-  $('#shoppingCart').sidebar('toggle');
-  $('#itemHeader').text('Pyramid ring');
-  $('#itemMaker').text('Perez Bitan');
-  $('#itemPrice').text('150.99$');
+  //$('#shoppingCart').first().sidebar('attach events', '#bagButton');
+  //$('#bagButton').removeClass('disabled');
+  $('#shoppingCart').sidebar('show');
+  $('#itemModal').modal('hide');
+  $('#itemHeader').text('Skeleton ring');
+  $('#itemMaker').text('Louise Collection');
+  $('#itemPrice').text('250.00$');
   $('#cartQuantity').text('1');
 }
 
 function checkout(){
-  $('#paymentModal').modal({blurring: false}).modal('show');
+  $('#paymentModal').modal('show');
 }
